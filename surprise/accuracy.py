d7618df6840aae8a98dd9b5a21fad9ee24e2abc1
damienos96 <damiandebny@hotmail.com>
2022-08-01 15:22:08 +0100

    Added NDCG to metrics

--- surprise/accuracy.py
+++ surprise/accuracy.py
@@ -122,6 +122,79 @@ def mae(predictions, verbose=True):
 
     return mae_
 
+def _dcg(scores, discount=np.log2):
+    """
+    Compute the Discounted Cumulative Gain of a series of recommended items with rating scores.
+    These should be relevance scores; they can be :math:`{0,1}` for binary relevance data.
+    This is not a true top-N metric, but is a utility function for other metrics.
+    Args:
+        scores(array-like):
+            The utility scores of a list of recommendations, in recommendation order.
+        discount(ufunc):
+            the rank discount function.  Each item's score will be divided the discount of its rank,
+            if the discount is greater than 1.
+    Returns:
+        double: the DCG of the scored items.
+    """
+    scores = np.nan_to_num(scores)
+    ranks = np.arange(1, len(scores) + 1)
+    disc = discount(ranks)
+    np.maximum(disc, 1, out=disc)
+    np.reciprocal(disc, out=disc)
+    return np.dot(scores, disc)
+
+def ndcg(recs, truth, discount=np.log2, k=None):
+    """
+    Compute the normalized discounted cumulative gain :cite:p:`Jarvelin2002-xf`.
+    Discounted cumultative gain is computed as:
+    .. math::
+        \\begin{align*}
+        \\mathrm{DCG}(L,u) & = \\sum_{i=1}^{|L|} \\frac{r_{ui}}{d(i)}
+        \\end{align*}
+    Unrated items are assumed to have a utility of 0; if no rating values are provided in the
+    truth frame, item ratings are assumed to be 1.
+    This is then normalized as follows:
+    .. math::
+        \\begin{align*}
+        \\mathrm{nDCG}(L, u) & = \\frac{\\mathrm{DCG}(L,u)}{\\mathrm{DCG}(L_{\\mathrm{ideal}}, u)}
+        \\end{align*}
+    This metric has a bulk implementation.
+    Args:
+        recs: The recommendation list.
+        truth: The user's test data.
+        discount(numpy.ufunc):
+            The rank discount function.  Each item's score will be divided the discount of its rank,
+            if the discount is greater than 1.
+        k(int):
+            The maximum list length.
+            
+    recs -> predictions
+    
+    """
+
+    tpos = truth.index.get_indexer(recs['item'])
+
+    if k is not None:
+        recs = recs.iloc[:k]
+
+    if 'rating' in truth.columns:
+        i_rates = np.sort(truth.rating.values)[::-1]
+        if k is not None:
+            i_rates = i_rates[:k]
+        ideal = _dcg(i_rates, discount)
+        # make an array of ratings for this rec list
+        r_rates = truth['rating'].values[tpos]
+        r_rates[tpos < 0] = 0
+        achieved = _dcg(r_rates, discount)
+    else:
+        ntrue = len(truth)
+        if k is not None and ntrue > k:
+            ntrue = k
+        ideal = _fixed_dcg(ntrue, discount)
+        tgood = tpos >= 0
+        achieved = _dcg(tgood, discount)
+
+    return achieved / ideal
 
 def fcp(predictions, verbose=True):
     """Compute FCP (Fraction of Concordant Pairs).
